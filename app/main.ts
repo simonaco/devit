394d13e5a7bf5fa25e23dc92edb7757104be1e58
simonaco <simona.cotin@gmail.com>
2016-05-13 08:52:28 +0100

    Empty component

---
+++ app/main.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+@Component({
+    
+})
+
+export /**
+ * AppComponent
+ */
+class AppComponent {
+    constructor() {
+        
+    }
+}
+
+bootstrap(AppComponent)
+
