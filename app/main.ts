e67ac0281e700061082833e704219c27cd8e857c
simonaco <simona.cotin@gmail.com>
2016-05-13 08:56:37 +0100

    Add template for simple component

--- app/main.ts
+++ app/main.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
 
 @Component({
-    
+    selector:"my-app",
+    template:"Hello Thessaloniki!"
 })
 
 export /**
